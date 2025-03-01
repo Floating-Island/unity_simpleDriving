5701217bcf30e23bb8a72c2c2c707bba0f1916bc
Alber <betomiku@gmail.com>
2025-03-01 23:38:28 +0100

    Add score system logic.

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,7 +1,14 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreGain = 1f;
+
+    private float score;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,17 @@ public class ScoreSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        IncreaseScore();
+        DisplayScore();
+    }
+
+    private void DisplayScore()
+    {
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void IncreaseScore()
+    {
+        score += (float)scoreGain * Time.deltaTime;
     }
 }
